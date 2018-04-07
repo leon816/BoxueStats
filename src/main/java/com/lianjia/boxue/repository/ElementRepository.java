3de4f610348211f9d0baa134f8c0987da244859e
liliang816 <liliangonly@qq.com>
2018-04-07 14:48:41 +0800

    删除ElementEntity

--- src/main/java/com/lianjia/boxue/repository/ElementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lianjia.boxue.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lianjia.boxue.entity.ElementEntity;
-
-public interface ElementRepository extends JpaRepository<ElementEntity, String> {
-
-}
